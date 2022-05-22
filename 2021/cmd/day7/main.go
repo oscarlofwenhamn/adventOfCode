2904d165fb3d35c5dc4db473ecdb4e1a23e09d8b
Oscar Löfwenhamn <oscar.lofwenhamn@trustly.com>
2022-05-22 20:52:05 +0200

    Finish day7 part 2
    I feel dirty

--- 2021/cmd/day7/main.go
+++ 2021/cmd/day7/main.go
@@ -19,12 +19,15 @@ func main() {
 		floats = append(floats, i)
 	}
 	println(len(floats))
-	fuelConsumption := alignCrabs(floats)
+	fuelConsumption := alignCrabsMedian(floats)
 
-	fmt.Printf("Total fuel consumption: %v\n", fuelConsumption)
+	fmt.Printf("Total fuel consumption for median: %v\n", fuelConsumption)
+
+	fuelConsumption = alignCrabsMean(floats)
+	fmt.Printf("Total fuel consumption for mean: %v\n", fuelConsumption)
 }
 
-func alignCrabs(startingPositions []float64) float64 {
+func alignCrabsMedian(startingPositions []float64) float64 {
 	sort.Float64s(startingPositions)
 
 	median := getMedian(startingPositions)
@@ -36,6 +39,29 @@ func alignCrabs(startingPositions []float64) float64 {
 	}
 
 	return fuelForMedian
+}
+
+func alignCrabsMean(startingPositions []float64) float64 {
+	sort.Float64s(startingPositions)
+
+	mean := getMean(startingPositions)
+	fmt.Printf("Mean: %v\n", mean)
+
+	meanFloor := math.Floor(mean)
+	var fuelForMeanFloor float64
+	for _, v := range startingPositions {
+		fuelForMeanFloor += fuel(math.Abs(v - meanFloor))
+	}
+	meanCeil := math.Ceil(mean)
+	var fuelForMeanCeil float64
+	for _, v := range startingPositions {
+		fuelForMeanCeil += fuel(math.Abs(v - meanCeil))
+	}
+
+	if fuelForMeanCeil < fuelForMeanFloor {
+		return fuelForMeanCeil
+	}
+	return fuelForMeanFloor
 }
 
 func getMedian(a []float64) float64 {
@@ -51,4 +77,11 @@ func getMean(a []float64) float64 {
 		sum += v
 	}
 	return sum / float64(len(a))
+}
+
+func fuel(n float64) float64 {
+	if n == 0 {
+		return 0
+	}
+	return n + fuel(n-1)
 }
--- 2021/cmd/day7/main_test.go
+++ 2021/cmd/day7/main_test.go
@@ -24,7 +24,7 @@ func Test_X(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			totalFuel := alignCrabs(tt.args.input)
+			totalFuel := alignCrabsMedian(tt.args.input)
 			if totalFuel != tt.result {
 				t.Fatalf("Lanternfish count not correct.\nExpected:\n%v\nActual:\n%v\n",
 					tt.result,
