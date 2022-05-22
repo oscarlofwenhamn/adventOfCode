3819cd137b991a16194bb00283cf58f0314a5556
Oscar Löfwenhamn <oscar.lofwenhamn@trustly.com>
2022-05-22 20:35:58 +0200

    Complete day 7 part 1
    This feels like a sort of arbitrary solution, not sure I like it.
    Solving part 2 with a mean feels off, not sure if it failed due to bad
    implementation or wrong solution. Too tired to figure it out.

--- 2021/cmd/day7/main.go
+++ 2021/cmd/day7/main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
 
+	"github.com/oscarlofwenhamn/adventOfCode/cmd/utils"
+)
+
+func main() {
+	input := strings.Split(utils.ReadStringArrayFromFile("./input")[0], ",")
+	fmt.Println(input)
+	floats := []float64{}
+	for _, v := range input {
+		i, _ := strconv.ParseFloat(v, 64)
+		floats = append(floats, i)
+	}
+	println(len(floats))
+	fuelConsumption := alignCrabs(floats)
+
+	fmt.Printf("Total fuel consumption: %v\n", fuelConsumption)
+}
+
+func alignCrabs(startingPositions []float64) float64 {
+	sort.Float64s(startingPositions)
+
+	median := getMedian(startingPositions)
+	fmt.Printf("Median: %v\n", median)
+
+	var fuelForMedian float64
+	for _, v := range startingPositions {
+		fuelForMedian += math.Abs(v - median)
+	}
+
+	return fuelForMedian
+}
+
+func getMedian(a []float64) float64 {
+	if len(a)%2 == 0 {
+		return (a[(len(a)-1)/2] + a[(len(a)+1)/2]) / 2
+	}
+	return a[len(a)/2]
+}
+
+func getMean(a []float64) float64 {
+	var sum float64
+	for _, v := range a {
+		sum += v
+	}
+	return sum / float64(len(a))
 }
--- 2021/cmd/day7/main_test.go
+++ 2021/cmd/day7/main_test.go
@@ -7,24 +7,29 @@ import (
 func Test_X(t *testing.T) {
 
 	type args struct {
-		input []int
+		input []float64
 	}
 	tests := []struct {
 		name   string
 		args   args
-		result int
+		result float64
 	}{
 		{
 			name: "Validate test input",
 			args: args{
-				input: []int{16, 1, 2, 0, 4, 2, 7, 1, 2, 14},
+				input: []float64{16, 1, 2, 0, 4, 2, 7, 1, 2, 14},
 			},
 			result: 37,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
+			totalFuel := alignCrabs(tt.args.input)
+			if totalFuel != tt.result {
+				t.Fatalf("Lanternfish count not correct.\nExpected:\n%v\nActual:\n%v\n",
+					tt.result,
+					totalFuel)
+			}
 		})
 	}
 }
