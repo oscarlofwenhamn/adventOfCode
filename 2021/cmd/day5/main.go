ed88ec5ec0476615f3dc69d7b333eb177ee1d8fd
Oscar Löfwenhamn <oscar.lofwenhamn@gmail.com>
2022-10-28 17:44:03 +0200

    Begin revisiting day 5
    Do things more piece by piece, adding unit tests to each step to ensure
    expected results.
    Done: Convert entry to list of coordinate pairs
    TODO: Use list of coordinates to draw diagram and count overlaps

--- 2021/cmd/day5/main.go
+++ 2021/cmd/day5/main.go
@@ -1,57 +1,66 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"regexp"
 	"strconv"
-	"strings"
-
-	"github.com/oscarlofwenhamn/adventOfCode/cmd/utils"
 )
 
+type vent struct {
+	level *int
+}
+
+func (v vent) getLevelValue() string {
+	switch *v.level {
+	case 0:
+		return "."
+	default:
+		return strconv.Itoa(*v.level)
+	}
+}
+
+func (v vent) increaseLevel() {
+	*v.level++
+}
+
+type pair struct {
+	x int
+	y int
+}
+
+func convertEntry(entry string) (ret []pair, err error) {
+
+	var indexes = regexp.MustCompile(`\d`)
+
+	var matches = indexes.FindAllString(entry, -1)
+
+	var xStart, xEnd, yStart, yEnd int
+
+	var results = [4]*int{&xStart, &yStart, &xEnd, &yEnd}
+	for i, v := range matches {
+		conv, err := strconv.Atoi(v)
+
+		if err != nil {
+			return nil, errors.New("entry conversion failed")
+		}
+
+		*results[i] = conv
+	}
+
+	for i := xStart; i <= xEnd; i++ {
+		for j := yStart; j <= yEnd; j++ {
+			ret = append(ret, pair{i, j})
+		}
+	}
+
+	return
+}
+
+func drawDiagram() [][]vent {
+	return [][]vent{}
+}
+
 func main() {
-
-	inputArray := utils.ReadStringArrayFromFile("data/input")
-
-	createDiagram(inputArray)
-}
-
-func createDiagram(input []string) []string {
-
-	matrix := [10][10]int{}
-
-	for _, line := range input {
-		//0,9 -> 5,9
-		matrix = convertSegment(line, matrix)
-	}
-
-	return input
-}
-
-func convertSegment(segment string, matrix [10][10]int) [10][10]int {
-	coordinates := strings.Split(segment, " -> ")
-	startX, startY := func(arr []string) (int, int) {
-		x, _ := strconv.Atoi(arr[0])
-		y, _ := strconv.Atoi(arr[1])
-		return x, y
-	}(strings.Split(coordinates[0], ","))
-	endX, endY := func(arr []string) (int, int) {
-		x, _ := strconv.Atoi(arr[0])
-		y, _ := strconv.Atoi(arr[1])
-		return x, y
-	}(strings.Split(coordinates[1], ","))
-
-	switch {
-	case startY == endY:
-		println("Horizontal line")
-		dist := endX - startX
-		for i := 0; i < dist; i++ {
-			matrix[startX+i][startY]++
-		}
-	case startX == endX:
-		println("Vertical line")
-		dist := endY - startY
-		for i := 0; i < dist; i++ {
-			matrix[startX][startY+i]++
-		}
-	}
-	return matrix
+	fmt.Println("Hello, World!")
 }
--- 2021/cmd/day5/main_test.go
+++ 2021/cmd/day5/main_test.go
@@ -1,27 +1,111 @@
 package main
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/oscarlofwenhamn/adventOfCode/cmd/utils"
 )
 
-// TestGenerateDiagram validates that createDiagram correctly generates the expected diagram based on test input
-func TestGenerateDiagram(t *testing.T) {
+func Test_vent_getLevel(t *testing.T) {
+	type fields struct {
+		level int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{name: "Returns . if level equals 0", fields: fields{0}, want: "."},
+		{name: "Returns string of level if level not equals 0", fields: fields{4}, want: "4"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := vent{
+				level: &tt.fields.level,
+			}
+			got := v.getLevelValue()
+			if got != tt.want {
+				t.Errorf("vent.getLevel() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-	inputArray := utils.ReadStringArrayFromFile("data/test_input")
-	outputArray := utils.ReadStringArrayFromFile("data/test_output")
+func Test_vent_increaseLevel(t *testing.T) {
+	type fields struct {
+		level int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{name: "Inceases level", fields: fields{level: 0}, want: 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := vent{
+				level: &tt.fields.level,
+			}
+			v.increaseLevel()
+			if *v.level != tt.want {
+				t.Errorf("vent.level = %v, want %v", *v.level, tt.want)
+			}
+		})
+	}
+}
 
-	diagram := createDiagram(inputArray)
+func Test_convertEntry(t *testing.T) {
+	type args struct {
+		entry string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantRet []pair
+		wantErr bool
+	}{
+		{name: "Converts on X axis", args: args{"0,0 -> 2,0"}, wantRet: []pair{{0, 0}, {1, 0}, {2, 0}}, wantErr: false},
+		{name: "Converts on Y axis", args: args{"0,0 -> 0,2"}, wantRet: []pair{{0, 0}, {0, 1}, {0, 2}}, wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotRet, err := convertEntry(tt.args.entry)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("convertEntry() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotRet, tt.wantRet) {
+				t.Errorf("convertEntry() = %v, want %v", gotRet, tt.wantRet)
+			}
+		})
+	}
+}
 
-	for i, line := range outputArray {
-		if diagram[i] != line {
-			t.Fatalf("Diagram line %d does not match desired output.\nExpected:\n%v\nActual:\n%v",
-				i+1,
-				line,
-				diagram[i])
+func Test_drawDiagram(t *testing.T) {
+	tests := []struct {
+		name string
+		want [][]vent
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := drawDiagram(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("drawDiagram() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
 
-		}
-
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
 	}
 }
